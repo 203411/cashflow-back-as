7caec00e8108ad9a350c38141a664cd68351c2bc
203424 <203424@ids.upchiapas.edu.mx>
2022-03-05 23:42:10 -0600

    First commit

--- cashflow_back_as/settings.py
+++ cashflow_back_as/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1(f(lfalpioeq#b87^#7^rgcd=km07_1+5mf$5b_1nrb5o!zup
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
