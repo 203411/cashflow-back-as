6848e266d2cd49dc9665b92ff27aad914d87c76d
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2022-03-14 20:54:37 -0600

    Relacion Uno a Muchos Categorias-FlujoEfectivo

--- FlujoEfectivo/views.py
+++ FlujoEfectivo/views.py
@@ -5,24 +5,35 @@ from rest_framework import status
 from FlujoEfectivo.serializers import FlujoSerializer
 from FlujoEfectivo.models import FlujoModel
 from datetime import datetime
+from Categorias.models import CategoriasModel
 
 # Create your views here.
 
 class FlujoView(APIView):
+    def get_objectCategoria(self, pk, format = None):
+        try:
+            CategoriasModel.objects.get(pk = pk)
+        except CategoriasModel.DoesNotExist:
+            return 404
+            
     def get(self, request, format = None):
         queryset = FlujoModel.objects.all()
         serializer = FlujoSerializer(queryset, many = True, context = {'request':request})
         return Response(serializer.data, status = status.HTTP_200_OK)
     
     def post(self, request, format = None):
-        request.data['fecha'] = datetime.now().strftime("%Y/%m/%d")
-        serializer = FlujoSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-    
+        categoria = self.get_objectCategoria(request.data['id_categoria'])
+        if(categoria != 404):
+            request.data['fecha'] = datetime.now().strftime("%Y/%m/%d")
+            serializer = FlujoSerializer(data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status = status.HTTP_201_CREATED)
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("Categoria No existe")
 class FlujoViewDetail(APIView):
+        
     def get_object(self,pk):
         try:
             return FlujoModel.objects.get(pk=pk)
