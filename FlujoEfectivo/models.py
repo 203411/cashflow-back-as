70e98f26072ced61b26801f383434f8ded6a5fad
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2022-03-15 17:26:56 -0600

    Correcciones en FlujoEfectivo e IndicadoresDinero

--- FlujoEfectivo/models.py
+++ FlujoEfectivo/models.py
@@ -1,19 +1,10 @@
 from django.db import models
-<<<<<<< HEAD
-=======
 from Categorias.models import CategoriasModel
->>>>>>> Develop-Gabriel
 
 # Create your models here.
 class FlujoModel(models.Model):
     tipo = models.CharField(max_length = 50, null = False)
-<<<<<<< HEAD
-    id_categoria = models.IntegerField(null = False)
-    descripcion = models.CharField(max_length = 50, null = False)
-    cantidad = models.IntegerField( null= False)
-=======
     id_categoria = models.ForeignKey(CategoriasModel, on_delete = models.CASCADE, null = False, blank = False)
     descripcion = models.CharField(max_length = 50, null = False)
     cantidad = models.IntegerField( null = False)
     fecha = models.CharField(max_length = 50, null = False, default="")
->>>>>>> Develop-Gabriel
