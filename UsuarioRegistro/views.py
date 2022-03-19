2c76b79ce7933a3323172bd48f8ad0b62bcd857c
203467-ids <203467@ids.upchiapas.edu.mx>
2022-03-18 21:04:28 -0600

    arreglado el password en register

--- UsuarioRegistro/views.py
+++ UsuarioRegistro/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
-
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 from rest_framework.decorators import api_view, permission_classes
@@ -48,7 +48,9 @@ def user_detail(request, pk):
             usuario_data.update(password2=User.objects.filter(pk=pk).values()[0]["password"])
         serializer = RegisterSerializer(usuario, data=usuario_data) 
         if serializer.is_valid(): 
-            serializer.save() 
+            if ('password' in usuario_data):
+                password = make_password(usuario_data['password'])
+            serializer.save(password=password) 
             return JsonResponse(serializer.data) 
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
