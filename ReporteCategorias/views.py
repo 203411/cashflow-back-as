2189c345b291fb133191caa1af809cad804764a6
203424 <203424@ids.upchiapas.edu.mx>
2022-03-21 19:02:41 -0600

    Actualizacion

--- ReporteCategorias/views.py
+++ ReporteCategorias/views.py
@@ -14,6 +14,8 @@ class ReporteCategoriasView(APIView):
         dataF = FlujoModel.objects.all()
         dataC = CategoriasModel.objects.all()
         semana = 0
+        registros = False
+        sumaCantidades = 0
         result = []
         ingresos = [
         [0,0,0,0,0], #fila de efectivo
@@ -33,40 +35,52 @@ class ReporteCategoriasView(APIView):
             dia = int(f.fecha[3:5])
             mes = int(f.fecha[:2])
 
-            if(dia < 8 and mes == mesUser):
-                semana = 0
-            if ((dia > 7 and dia < 15) and mes == mesUser):
-                semana = 1
-            if((dia>14 and dia < 22) and mes == mesUser):
-                semana = 2
-            if((dia>21) and mes == mesUser):
-                semana = 3
+            if(mes == mesUser):
+                if(dia < 8 and mes == mesUser):
+                    semana = 0
+                if ((dia > 7 and dia < 15) and mes == mesUser):
+                    semana = 1
+                if((dia>14 and dia < 22) and mes == mesUser):
+                    semana = 2
+                if((dia>21) and mes == mesUser):
+                    semana = 3
 
-            if(subCategoria == "EFECTIVO"):
-                ingresos[0][semana] += f.cantidad
-            elif(subCategoria == "DEPOSITO"):
-                ingresos[1][semana] += f.cantidad
-            ingresos[2][semana] = ingresos[0][semana] + ingresos[1][semana]
+                if(subCategoria == "EFECTIVO"):
+                    ingresos[0][semana] += f.cantidad
+                elif(subCategoria == "DEPOSITO"):
+                    ingresos[1][semana] += f.cantidad
+                ingresos[2][semana] = ingresos[0][semana] + ingresos[1][semana]
 
-            if(categoria == "COSTO-VENTA"):
-                gastos[0][semana] += f.cantidad
-            elif(categoria == "GASTO-AOC"):
-                gastos[1][semana] += f.cantidad
-            gastos[2][semana] = gastos[0][semana] + gastos[1][semana]      
+                if(categoria == "COSTO-VENTA"):
+                    gastos[0][semana] += f.cantidad
+                elif(categoria == "GASTO-AOC"):
+                    gastos[1][semana] += f.cantidad
+                gastos[2][semana] = gastos[0][semana] + gastos[1][semana]      
+        
+        for registro in ingresos: #verificamos que exista almenos un registro sumando las cantidades, en caso de que no existan no se hará un reporte
+            sumaCantidades = sum(registro)
+        for registro in gastos:
+            sumaCantidades += sum(registro)
+        if(sumaCantidades>0):
+            registros = True
+        else:
+            registros = False
 
-        for i in [0,1,2]: #indices de las filas que hay
-            for cantidad in ingresos[i][0:4]: #buscar cada elemento de la fila
-                ingresos[i][4] += cantidad #hacer el total de los elementos de esa fila y guardarlo al final de la misma
-            for cantidad in gastos[i][0:4]: #buscar cada elemento de la fila
-                gastos[i][4] += cantidad #hacer el total de los elementos de esa fila y guardarlo al final de la misma
-        for i in [0,1,2,3,4]:
-            diferencia[i] = ingresos[2][i] - gastos[2][i]
-            rentabilidad[i] = round((diferencia[i]*100)/ingresos[2][i])
-        result.append({
-                'cantidadEntrada' : ingresos,
-                'cantidadSalida' : gastos,
-                'utilidad' : diferencia,
-                'rentabilidad' : rentabilidad
-            })
-
-        return JsonResponse(result, safe=False)
+        if(registros == True):
+            for i in [0,1,2]: #indices de las filas que hay
+                for cantidad in ingresos[i][0:4]: #buscar cada elemento de la fila
+                    ingresos[i][4] += cantidad #hacer el total de los elementos de esa fila y guardarlo al final de la misma
+                for cantidad in gastos[i][0:4]: #buscar cada elemento de la fila
+                    gastos[i][4] += cantidad #hacer el total de los elementos de esa fila y guardarlo al final de la misma
+            for i in [0,1,2,3,4]:
+                diferencia[i] = ingresos[2][i] - gastos[2][i]
+                rentabilidad[i] = round((diferencia[i]*100)/ingresos[2][i])
+            result.append({
+                    'cantidadEntrada' : ingresos,
+                    'cantidadSalida' : gastos,
+                    'utilidad' : diferencia,
+                    'rentabilidad' : rentabilidad
+                })
+            return JsonResponse(result, safe=False)
+        else:
+            return JsonResponse({"message" : "No hay registros del mes seleccionado"}, safe=False)   
